6e0dc03301ddba90bdc48b26dc3c2153e819a479
Theneva <splugs@gmail.com>
2014-08-30 23:26:02 +0200

    Select card to add to deck if multiple cards match.

--- src/view/DeckTrackingView.java
+++ src/view/DeckTrackingView.java
@@ -2,6 +2,7 @@ package view;
 
 import middle.GetCardsByBeginningOfNameRequest;
 import model.Card;
+import view.util.NoSuchCardException;
 import view.util.ObservableJFrame;
 
 import javax.swing.*;
@@ -10,8 +11,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Vector;
 
 public class DeckTrackingView extends JFrame implements Observer, ObservableJFrame
 {
@@ -88,20 +91,10 @@ public class DeckTrackingView extends JFrame implements Observer, ObservableJFra
             if (cards.size() == 1)
             {
                 final Card card = cards.get(0);
-                deckListData.add(card.getName() + " | " + card.getAttack() + "-" + card.getHealth());
-
-                deckList.setListData(deckListData);
-
-                // Focus the newest entry.
-                deckList.setSelectedIndex(deckListData.size() - 1);
-
-                deckList.revalidate();
-                deckList.repaint();
+                addCardToDeck(card);
             }
             else
             {
-
-                // Build an array of the matching card names (and reverse the list for the options to go alphabetical).
                 final String[] cardNames = new String[cards.size()];
 
                 for (int i = 0; i < cards.size(); i++)
@@ -111,7 +104,7 @@ public class DeckTrackingView extends JFrame implements Observer, ObservableJFra
                 }
 
                 // TODO: Actually display these to the user.
-                JOptionPane.showOptionDialog(
+                final int selectedIndex = JOptionPane.showOptionDialog(
                         DeckTrackingView.this,
                         "Please select a card",
                         "Cards matching the input",
@@ -119,8 +112,38 @@ public class DeckTrackingView extends JFrame implements Observer, ObservableJFra
                         cardNames,
                         0
                 );
+
+                final String selectedCardName = cardNames[selectedIndex];
+
+                addCardToDeck(findCardByName(cards, selectedCardName));
             }
         }
+    }
+
+    private Card findCardByName(final List<Card> cards, final String selectedCardName)
+    {
+        for (final Card card : cards)
+        {
+            if (card.getName().equals(selectedCardName))
+            {
+                return card;
+            }
+        }
+
+        throw new NoSuchCardException();
+    }
+
+    private void addCardToDeck(final Card card)
+    {
+        deckListData.add(card.getName() + " | " + card.getAttack() + "-" + card.getHealth());
+
+        deckList.setListData(deckListData);
+
+        // Focus the newest entry.
+        deckList.setSelectedIndex(deckListData.size() - 1);
+
+        deckList.revalidate();
+        deckList.repaint();
     }
 
     @Override
---
+++ src/view/util/NoSuchCardException.java
@@ -0,0 +1,5 @@
+package view.util;
+
+public class NoSuchCardException extends RuntimeException
+{
+}
